9e28048e6678014d4390c01bbefee76eb19813f8
gabi <annagabsoares@gmail.com>
2021-09-16 16:41:02 -0700

    commit updates

--- grocery_store/product.py
+++ grocery_store/product.py
@@ -2,10 +2,8 @@
 def create_product(name, price):
     return {
         "name": name,
-        "price": name,
+        "price": price,
     }
 
 def print_product(product):
     return f"{product['name']} - ${product['price']}"
-    
-
